e878b4615859b86ab97d0297a27806cce79f2aa1
Evelyn Sofia Ruiz Mendoza <a01716238@tec.mx>
2026-09-06 20:11:05 -0600

    Update proyect.py

--- proyect.py
+++ proyect.py
@@ -1,19 +1,19 @@
 #Sistema de gestión y seguimiento de proyectos
 def crear_cuenta():
-print("REGISTRTO NUEVA CUENTA")
-nuevo_usuario = input("Ingrese un nombre de usuario")
-nueva_contrasena = input("Ingrese una contraseña")
-if nuevo_usuario and nueva_contrasena:
-print("¡Cuenta de '{nuevo_usuario}' registrada con éxito!")
-else:
-print("Error: Debe llenar todos los campos")
+  print("REGISTRTO NUEVA CUENTA")
+  nuevo_usuario = input("Ingrese un nombre de usuario")
+  nueva_contrasena = input("Ingrese una contraseña")
+    if nuevo_usuario and nueva_contrasena:
+  print("¡Cuenta de '{nuevo_usuario}' registrada con éxito!")
+    else:
+  print("Error: Debe llenar todos los campos")
 
 def iniciar_sesion():
   print("Inicio de sesión")
   usuario = input("Ingrese su usario:")
   contrasena = input("Ingrese su contraseña: ")
-  if usuario and contrasena:
-    print("¡Bienvenido/a, '{usuario}'!")
+    if usuario and contrasena:
+  print("¡Bienvenido/a, '{usuario}'!")
     return True
   else:
     print("Datos incorrectos")
@@ -24,7 +24,7 @@ def registrar_proyecto():
   nombre = input ("Nombre del proyecto")
   try:
     avance = float(input("Porcentaje de avance (0-100)")
-    if 0 <= avance <= 100:
+  if 0 <= avance <= 100:
        print("Proyecto '{nombre}' registrado con {avance: .f}% de avance")
   else:
         print("El porcentaje debe estar entre 0 y 100")
@@ -34,29 +34,29 @@ except ValueError:
 def calcular_metricas():
         print("Cálculo de métricas de tiempo y financieras")
   nombre = input("Nombre del proyecto:")
-try:
-  metas_total = int(input("Total de metas planificadas:"))
-  metas_cumplidas = int(input("Metas alcanzadas:"))
-  presupuesto_inicial = int(input("Presupuesto inicial($):"))
-  presupuesto_gastado = int(input("Presupuesto gastado($):"))
-  dias = int(input("Duración estimada en días del proyecto:"))}
+  try:
+    metas_total = int(input("Total de metas planificadas:"))
+    metas_cumplidas = int(input("Metas alcanzadas:"))
+    presupuesto_inicial = int(input("Presupuesto inicial($):"))
+    presupuesto_gastado = int(input("Presupuesto gastado($):"))
+    dias = int(input("Duración estimada en días del proyecto:"))}
 
-  if metas_total <=0:
-    print("Error: Sus metas al menos debe de ser de 1")
-    return
+    if metas_total <=0:
+      print("Error: Sus metas al menos debe de ser de 1")
+      return
 
-    presupuesto_restante = presupuesto_inicial - presupuesto_gastado
-    porcentaje_avance = (metas_cumplidas / metas_total) * 100
-    semanas = dias // 7
-    dias_s = dias % 7
+      presupuesto_restante = presupuesto_inicial - presupuesto_gastado
+      porcentaje_avance = (metas_cumplidas / metas_total) * 100
+      semanas = dias // 7
+      dias_s = dias % 7
 
     print("Resumen de métricas: {nombre}")
     print("Avance real de metas: {porcentaje_avance:.2f}%")
     print("Presupuesto disponible: {presupuesto_restante:.2f}%")
     print("Tiempo estimado: {semanas} semanas y {dias_s} dias")
 
-except ValueError:
-print("Error: Ingrese datos válidos")
+  except ValueError:
+    print("Error: Ingrese datos válidos")
 
 def mostrar_menu():
   print("Sistema de gestión de proyectos y resultados")
@@ -68,20 +68,20 @@ def mostrar_menu():
 
 def main():
   ejecutando = True
-while ejecutando:
-  mostrar_menu()
-opcion = input("Selecciona una opción (1-5):")
-if opcion == "1":
-  crear_cuenta()
-elif opcion == "2":
-iniciar_sesion()
-elif opcion == "3":
-registrar_proyecto()
-elif opcion == "4":
-calcular_metricas()
-elif opcion == "5":
-print("Cerrando el programa")
-ejecutando = False
-else: 
-print("Opción no válida")
-main()
+    while ejecutando:
+      mostrar_menu()
+        opcion = input("Selecciona una opción (1-5):")
+    if opcion == "1":
+      crear_cuenta()
+    elif opcion == "2":
+      iniciar_sesion()
+    elif opcion == "3":
+      registrar_proyecto()
+    elif opcion == "4":
+      calcular_metricas()
+    elif opcion == "5":
+      print("Cerrando el programa")
+        ejecutando = False
+    else: 
+      print("Opción no válida")
+        main()
